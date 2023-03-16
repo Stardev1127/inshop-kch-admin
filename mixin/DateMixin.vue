0fb4a130a7b72031f36715b03b16257281d4e4b1
Stardev1127 <anthony.macdonald.dev1127@gmail.com>
2023-03-16 02:38:09 -0400

    first commit

---
+++ mixin/DateMixin.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+  methods: {
+    crmDateFormat(date, format = 'DD-MM-YYYY') {
+      if (date) {
+        return this.$date(date).format(format)
+      }
+    },
+  },
+}
+</script>
